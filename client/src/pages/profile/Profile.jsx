b47f372c71f56487a9c6df2f9747434658c8b51c
project-onbarding <20203024.it@gmail.com>
2024-02-04 00:26:20 +0530

    fix the profile post 400 error

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -3,9 +3,20 @@ import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Profile() {
+  const [user,setUser]=useState([]);
   const PF=process.env.REACT_APP_PUBLIC_FOLDER
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?username=Roy`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, []);
 
   return (
     <>
@@ -27,8 +38,8 @@ export default function Profile() {
               />
             </div>
             <div className="profileInfo">
-                <h4 className="profileInfoName">Safak Kocaoglu</h4>
-                <span className="profileInfoDesc">Hello my friends!</span>
+                <h4 className="profileInfoName">{user.username}</h4>
+                <span className="profileInfoDesc">{user.desc}</span>
             </div>
           </div>
           <div className="profileRightBottom">
